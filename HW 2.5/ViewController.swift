9d1aa0f6c1960e85c5e03483951b60c8a174e218
onixjk <onixjkk@gmail.ru>
2022-01-05 16:06:21 +0300

    Initial Commit

---
+++ HW 2.5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.5
+//
+//  Created by Onix qq on 5.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
